1d5e277bba33119a55e3a7f27fdbe8bb7d3ab699
Michał Sidor (Michcioperz) <michcioperz@autistici.org>
2014-05-17 20:24:23 +0200

    APIs, stuff, AJAX, stuff

--- michify8080.py
+++ michify8080.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, json
 import subprocess, random, sys
 import michispotify as spotify
 from alsaaudio import Mixer
@@ -7,12 +7,21 @@ app = Flask(__name__)
 spt = spotify.SpotifyController()
 mxr = Mixer()
 
+volumes = [0,0,0,0,0,0]
+volumes[0] = 0
+volumes[1] = 80
+volumes[2] = 85
+volumes[3] = 90
+volumes[4] = 95
+volumes[5] = 100
+
+
 password = random.randint(100000,999999)
 print(password)
 
 @app.route('/')
 def homepage():
-    return render_template('home.html', artist=spt.artist().decode(), album=spt.album().decode(), track=spt.title().decode(), css=url_for('static', filename='style.css'), js=url_for('static', filename='additional.js'), linkn=url_for('nexttrack'), linkp=url_for('prevtrack'), linkr=url_for('homepage'), linkt=url_for('toggleplay'), vol1=url_for('volume', volume=80), vol2=url_for('volume', volume=85), vol3=url_for('volume', volume=90), vol4=url_for('volume', volume=95), vol5=url_for('volume', volume=100), volm=url_for('volume', volume=0), volume=mxr.getvolume('playback')[0])
+    return render_template('home.html', artist=spt.artist().decode(), album=spt.album().decode(), track=spt.title().decode(), css=url_for('static', filename='style.css'), js=url_for('static', filename='additional.js'), linkn=url_for('apinext'), linkp=url_for('prevtrack'), linkr=url_for('homepage'), linkt=url_for('toggleplay'), vol1=url_for('volume', volume=volumes[1]), vol2=url_for('volume', volume=volumes[2]), vol3=url_for('volume', volume=volumes[3]), vol4=url_for('volume', volume=volumes[4]), vol5=url_for('volume', volume=volumes[5]), volm=url_for('volume', volume=volumes[0]), jsonstatusapi=url_for('apijsonstatus'), volume=mxr.getvolume('playback')[0])
 
 @app.route('/favicon.ico')
 def favicon():
@@ -42,6 +51,35 @@ def volume(volume):
     if volume <= 100 and volume >= 0:
         mxr.setvolume(volume)
     return redirect(url_for('homepage'))
+
+@app.route('/api/shortstatus')
+def apishortstatus():
+    return "%s - %s" % (spt.title().decode(), spt.artist().decode())
+
+@app.route('/api/jsonstatus')
+def apijsonstatus():
+    return json.dumps({'artist':spt.artist().decode(),'album':spt.album().decode(),'track':spt.title().decode()})
+
+@app.route('/api/toggle')
+def apitoggle():
+    spt.playpause()
+    return ""
+
+@app.route('/api/next')
+def apinext():
+    spt.forward()
+    return ""
+
+@app.route('/api/prev')
+def apiprev():
+    spt.previous()
+    return ""
+
+@app.route('/api/volume/<int:volume>')
+def apivolume(volume):
+    if volume <= 5 and volume >= 0:
+        mxr.setvolume(volumes[0])
+    return ""
 
 @app.route('/sd/<int:passcode>')
 def shutdown(passcode):
