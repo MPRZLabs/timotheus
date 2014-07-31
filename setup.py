392a96b1d5b1fd00700bdcc47cb4d7878154a837
Michał Sidor (Michcioperz) <michcioperz@autistici.org>
2014-07-31 23:07:28 +0200

    you auto-complete me

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='michi-timotheus',
-      version='1.1.1',
+      version='1.1.3',
       author='Michał Sidor',
       author_email='michcioperz@gmail.com',
       description='wrapper over TiMidity++ and FFmpeg',
