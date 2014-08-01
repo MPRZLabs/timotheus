105e60a708412070cc535dfaca39c5c9b8b4d28b
Michał Sidor (Michcioperz) <michcioperz@autistici.org>
2014-08-01 13:03:31 +0200

    consider it working. v1.3.3.7 now

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='michi-timotheus',
-      version='1.3.2',
+      version='1.3.3.7',
       author='Michał Sidor',
       author_email='michcioperz@gmail.com',
       description='wrapper over TiMidity++ and FFmpeg',
