24bd556906332ed40d4e52ca9a7fc542cf5641b6
Michał Sidor (Michcioperz) <michcioperz@autistici.org>
2014-08-01 15:24:55 +0200

    added regex for track matching and JSON config file for default regexes

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='michi-timotheus',
-      version='1.3.3.7',
+      version='1.4',
       author='Michał Sidor',
       author_email='michcioperz@gmail.com',
       description='wrapper over TiMidity++ and FFmpeg',
