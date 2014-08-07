ce3bbacd727a216c277cba59ccd167f79e057285
Michał Sidor (Michcioperz) <michcioperz@autistici.org>
2014-08-07 14:15:15 +0200

    version push

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='michi-timotheus',
-      version='1.4.1',
+      version='1.4.2',
       author='Michał Sidor',
       author_email='michcioperz@gmail.com',
       description='wrapper over TiMidity++ and FFmpeg',
