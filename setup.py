dbdbb4a7430a30968718ef8b4c384330b4b7a276
Michał Sidor (Michcioperz) <michcioperz@autistici.org>
2014-08-07 14:30:09 +0200

    now killing old player when starting new (just in case)

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='michi-timotheus',
-      version='1.4.2',
+      version='1.4.3',
       author='Michał Sidor',
       author_email='michcioperz@gmail.com',
       description='wrapper over TiMidity++ and FFmpeg',
