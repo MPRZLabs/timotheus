a12891e491da93988d43617b4d486526e34ab980
Michał Sidor (Michcioperz) <michcioperz@autistici.org>
2014-08-08 16:04:39 +0200

    version bump

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='michi-timotheus',
-      version='1.5',
+      version='1.5.99',
       author='Michał Sidor',
       author_email='michcioperz@gmail.com',
       description='wrapper over TiMidity++ and FFmpeg',
