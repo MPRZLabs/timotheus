1ab6537f1e3aa58fdd841e01fb99674b2a80b949
Michał Sidor (Michcioperz) <michcioperz@autistici.org>
2014-08-18 22:55:50 +0200

    replaced regexes stuff with tags

--- michimusemodels.py
+++ michimusemodels.py
@@ -30,4 +30,47 @@ class Track(Model):
   class Meta:
     database = db
 
-Track.create_table(fail_silently=True)
+class Tag(Model):
+  name = CharField()
+  def tag_by_query(self, query):
+    for track in query:
+      Tagged.create(track = track, tag = self)
+  def tag_by_artist(self, artist):
+    for track in Track.select().where(Track.artist.contains(artist)):
+      Tagged.create(track = track, tag = self)
+  def tag_by_path(self, path):
+    for track in Track.select().where(Track.path.contains(path)):
+      Tagged.create(track = track, tag = self)
+  class Meta:
+    database = db
+
+class Tagged(Model):
+  track = ForeignKeyField(Track)
+  tag = ForeignKeyField(Tag, related_name='marks')
+  class Meta:
+    database = db
+
+def hunt_tags():
+  for tag1 in Tagged.select():
+    print(tag1.id)
+    for tag2 in Tagged.select():
+      if tag1.tag == tag2.tag and tag1.track == tag2.track:
+        if tag1.id != tag2.id:
+          print("-", end='')
+          tag2.delete_instance()
+          return False
+        else:
+          print(":", end='')
+      else:
+        print(".", end='')
+    print()
+  return True
+
+if __name__ == '__main__':
+  Track.create_table(fail_silently=False)
+  Tag.create_table(fail_silently=False)
+  Tagged.create_table(fail_silently=False)
+else:
+  Track.create_table(fail_silently=True)
+  Tag.create_table(fail_silently=True)
+  Tagged.create_table(fail_silently=True)
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='michi-timotheus',
-      version='1.7.1',
+      version='1.7.5',
       author='Michał Sidor',
       author_email='michcioperz@gmail.com',
       description='wrapper over TiMidity++ and FFmpeg',
